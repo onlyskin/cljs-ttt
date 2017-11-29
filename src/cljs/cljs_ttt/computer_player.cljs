e3dd976c8a019627ae573a813b8dccb5857cc397
Sam Szreter <s.szreter@gmail.com>
2017-11-29 14:30:44 +0200

    First attempt at alphabeta pruning

--- src/cljs/cljs_ttt/computer_player.cljs
+++ src/cljs/cljs_ttt/computer_player.cljs
@@ -15,28 +15,49 @@
     (winner? board (current-marker board)) (winning-score)
     :else (losing-score)))
 
-(defn- max-by-score [scores]
-  (apply max-key val scores)) 
-
 (declare negamax)
-(defn make-nodes [board moves]
-  (let [nodes (->> moves
-                   (map #(->> %
-                              (play-on-board board)
-                              (negamax))))]
-    (map #(vector %1 (- (last %2))) moves nodes)))
+(defn f [state move]
+  (cond
+    (state :break) state
+    :else (let
+            [new-board (play-on-board (state :board) move)
+             best-node (negamax
+                         {:board new-board
+                          :best-move nil
+                          :best-score -10000
+                          :alpha (- (state :beta))
+                          :beta (- (state :alpha))
+                          :colour (- (state :colour))
+                          :break false})
+             best-score (max (last best-node) (state :best-score))
+             best-move (if (> (last best-node) (state :best-score)) (first best-node) move)
+             alpha (max (state :alpha) (last best-node))
+             break (>= (state :alpha) (state :beta))]
+            {:board (state :board)
+             :best-score best-score
+             :best-move best-move
+             :alpha alpha
+             :beta (state :beta)
+             :colour (state :colour)
+             :break break})))
 
-
-(defn negamax [board]
+(defn negamax [state]
   (cond
 
-    (game-over? board)
-    [nil (score board)]
+    (game-over? (state :board))
+    [nil (* (state :colour) (score (state :board)))]
 
     :else
-    (let [moves (available-moves board)
-          nodes (make-nodes board moves)]
-      (max-by-score nodes))))
+    (let [moves (available-moves (state :board))
+          result (reduce f state moves)]
+      [(result :best-move) (result :best-score)])))
 
 (defn get-negamax-move [board]
-  (first (negamax board)))
+  (let [initial-state {:board board
+                       :best-score -10000
+                       :best-move nil
+                       :alpha -10000
+                       :beta 10000
+                       :colour 1
+                       :break false}]
+    (first (negamax initial-state))))
